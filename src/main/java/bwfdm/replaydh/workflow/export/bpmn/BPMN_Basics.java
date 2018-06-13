911728272bbd9ebc9f261eee541172bb3b6edbec
Florian Fritze (UB) <florian.fritze@ub.uni-stuttgart.de>
2018-06-13 15:13:34 +0200

    added a comment

--- src/main/java/bwfdm/replaydh/workflow/export/bpmn/BPMN_Basics.java
+++ src/main/java/bwfdm/replaydh/workflow/export/bpmn/BPMN_Basics.java
@@ -41,7 +41,13 @@ public class BPMN_Basics {
 		  to.getIncoming().add(sequenceFlow);
 		  return sequenceFlow;
 	}
-	
+	/**
+	 * Creates an element in the process instance; copied from https://docs.camunda.org/manual/7.7/user-guide/model-api/bpmn-model-api/create-a-model/
+	 * @param parentElement
+	 * @param id
+	 * @param elementClass
+	 * @return
+	 */
 	protected <T extends BpmnModelElementInstance> T createElement(BpmnModelElementInstance parentElement, String id, Class<T> elementClass) {
 		  T element = modelInstance.newInstance(elementClass);
 		  element.setAttributeValue("id", id, true);
