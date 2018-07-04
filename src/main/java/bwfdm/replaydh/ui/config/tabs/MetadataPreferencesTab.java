2ab9ceeb271b65d1526f5113a09e4e6197b43a7e
Florian Fritze (UB) <florian.fritze@ub.uni-stuttgart.de>
2018-07-04 14:54:01 +0200

    some preparations for the metadata preferences tab

---
+++ src/main/java/bwfdm/replaydh/ui/config/tabs/MetadataPreferencesTab.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package bwfdm.replaydh.ui.config.tabs;
+
+import javax.swing.JCheckBox;
+
+import com.jgoodies.forms.builder.FormBuilder;
+
+import bwfdm.replaydh.core.RDHEnvironment;
+import bwfdm.replaydh.resources.ResourceManager;
+import bwfdm.replaydh.ui.config.DelegatingPreferencesTab;
+
+/**
+ * @author Florian Fritze
+ *
+ */
+public class MetadataPreferencesTab extends DelegatingPreferencesTab {
+	
+	public MetadataPreferencesTab(RDHEnvironment environment) {
+		ResourceManager rm = ResourceManager.getInstance();
+		
+		JCheckBox cbMetadataScope = new JCheckBox();
+		
+		FormBuilder.create()
+		.columns("left:pref, 15dlu, left:pref, fill:pref:grow")
+		.rows("pref, $nlg, pref, $lg, pref, 10dlu, pref, $nlg, pref, $lg, pref, $lg, pref")
+		.panel(getPanel())
+		.addLabel(rm.get("replaydh.plugins.metadataPreferencesTab.scope")).xy(1, 9).add(cbMetadataScope).xy(3, 9)
+		
+		.build();
+
+		
+	}
+}
