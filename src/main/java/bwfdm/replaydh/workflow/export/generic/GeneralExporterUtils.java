38ef9e0f537e12bef2afeab9d3e6e8e5783dac84
Volodymyr Kushnarenko <volodymyr.kushnarenko@uni-ulm.de>
2018-10-17 20:08:53 +0200

    move some common export utils to separate class

---
+++ src/main/java/bwfdm/replaydh/workflow/export/generic/GeneralExporterUtils.java
@@ -0,0 +1,37 @@
+package bwfdm.replaydh.workflow.export.generic;
+
+import org.swordapp.client.UriRegistry;
+
+public class GeneralExporterUtils {
+
+	/**
+	 * Get a file extension (without a dot) from the file name (e.g. "txt", "zip", ...)
+	 * @param fileName
+	 * @return
+	 */
+	public static String getFileExtension(String fileName) {	
+		String extension = "";
+		int i = fileName.lastIndexOf('.');
+		if(i>0) {
+			extension = fileName.substring(i+1);
+		}
+		return extension;		
+	}
+	
+	
+	/**
+	 * Get package format basing on the file name.
+	 * E.g. {@link UriRegistry.PACKAGE_SIMPLE_ZIP} {@link UriRegistry.PACKAGE_BINARY}
+	 * @param fileName
+	 * @return 
+	 */
+	public static String getPackageFormat(String fileName) {
+		String extension = getFileExtension(fileName);
+		
+		if(extension.toLowerCase().equals("zip")) {
+			return UriRegistry.PACKAGE_SIMPLE_ZIP;
+		}
+		return UriRegistry.PACKAGE_BINARY;
+	}
+	
+}
