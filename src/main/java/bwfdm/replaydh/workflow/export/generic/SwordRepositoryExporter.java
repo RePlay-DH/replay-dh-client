81dc5b59ab982c3e45bcd714b19965d8e903db08
Markus Gärtner <mcgaerty@ikarus-systems.net>
2018-10-18 14:50:33 +0200

    Prepare SwordRepositoryExporter and cleanup code

--- src/main/java/bwfdm/replaydh/workflow/export/generic/SwordRepositoryExporter.java
+++ src/main/java/bwfdm/replaydh/workflow/export/generic/SwordRepositoryExporter.java
@@ -1,5 +1,7 @@
 package bwfdm.replaydh.workflow.export.generic;
 
+import static java.util.Objects.requireNonNull;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,86 +27,77 @@ import org.swordapp.client.SwordResponse;
 import org.swordapp.client.UriRegistry;
 
 public class SwordRepositoryExporter {
-	
-	private AuthCredentials authCredentials = null;
-	
-	private SWORDClient swordClient;
-	
-	public SwordRepositoryExporter(AuthCredentials authCredentials) {
-		swordClient = new SWORDClient();
-		this.authCredentials = authCredentials;
-	}
-	
+
 	protected static final Logger log = LoggerFactory.getLogger(SwordRepositoryExporter.class);
-	
+
 	public static final String APPLICATION_JSON = "application/json";
 	public static final String CONTENT_TYPE_HEADER = "Content-Type";
 	public static final String ACCEPT_HEADER = "Accept";
 	public static final String MIME_FORMAT_ZIP = "application/zip";
 	public static final String MIME_FORMAT_ATOM_XML = "application/atom+xml";
-	
-	public static enum SwordRequestType {	
+
+	public static enum SwordRequestType {
 		DEPOSIT("DEPOSIT"), //"POST" request
 		REPLACE("REPLACE"), //"PUT" request
 		DELETE("DELETE")	//reserved for the future
 		;
-		
+
 		private final String label;
-		
+
 		private SwordRequestType(String label) {
 			this.label = label;
 		}
-		
+
 		public String getLabel() {
 			return label;
 		}
-		
+
 		@Override
 		public String toString() {
 			return label;
 		}
 	}
-	
-	
+
+
 	/**
 	 * Get a file extension (without a dot) from the file name (e.g. "txt", "zip", ...)
 	 * @param fileName
 	 * @return
 	 */
-	public static String getFileExtension(String fileName) {	
+	public static String getFileExtension(String fileName) {
 		String extension = "";
 		int i = fileName.lastIndexOf('.');
 		if(i>0) {
 			extension = fileName.substring(i+1);
 		}
-		return extension;		
+		return extension;
 	}
-	
-	
+
+
 	/**
 	 * Get package format basing on the file name.
 	 * E.g. {@link UriRegistry.PACKAGE_SIMPLE_ZIP} {@link UriRegistry.PACKAGE_BINARY}
 	 * @param fileName
-	 * @return 
+	 * @return
 	 */
 	public static String getPackageFormat(String fileName) {
 		String extension = getFileExtension(fileName);
-		
+
 		if(extension.toLowerCase().equals("zip")) {
 			return UriRegistry.PACKAGE_SIMPLE_ZIP;
 		}
 		return UriRegistry.PACKAGE_BINARY;
 	}
-		
-	
+
+
 	/**
 	 * Get available collections via SWORD v2
-	 * 
+	 *
 	 * @return Map<String, String> where key=URL, value=Title
 	 */
 	public static Map<String, String> getAvailableCollectionsViaSWORD(ServiceDocument serviceDocument){
 		Map<String, String> collections = new HashMap<String, String>();
-		
+
 		if(serviceDocument != null) {
 			for(SWORDWorkspace workspace : serviceDocument.getWorkspaces()) {
 				for (SWORDCollection collection : workspace.getCollections()) {
@@ -115,59 +108,74 @@ public class SwordRepositoryExporter {
 		}
 		return collections;
 	}
-	
+
+	private final AuthCredentials authCredentials;
+
+	private final SWORDClient swordClient;
+
+	protected SwordRepositoryExporter(AuthCredentials authCredentials) {
+		swordClient = new SWORDClient();
+		this.authCredentials = requireNonNull(authCredentials);
+	}
+
+	public AuthCredentials getAuthCredentials() {
+		return authCredentials;
+	}
+
+
+	protected SWORDClient getSwordClient() {
+		return swordClient;
+	}
+
+
 	public ServiceDocument getServiceDocument(String serviceDocumentURL) {
 		ServiceDocument serviceDocument = null;
 		try {
 			serviceDocument = swordClient.getServiceDocument(serviceDocumentURL, authCredentials);
 		} catch (SWORDClientException | ProtocolViolationException e) {
-			log.error("Exception by accessing service document: " + e.getClass().getSimpleName() + ": " + e.getMessage());
+			log.error("Exception by accessing service document", e);
 			return null;
 		}
 		return serviceDocument;
 	}
-	
+
 	/**
 	 * Check if SWORDv2-protocol is accessible
 	 * @return
 	 */
 	public boolean isSwordAccessible(String serviceDocumentURL) {
-		if(this.getServiceDocument(serviceDocumentURL) != null) {
-			return true;
-		} else {
-			return false;
-		}
+		return getServiceDocument(serviceDocumentURL) != null;
 	}
-	
+
 	/**
 	 * Publish a file or metadata. Private method.
 	 * <p>
-	 * IMPORTANT - you can use ONLY 1 possibility in the same time (only file, or only metadata). 
+	 * IMPORTANT - you can use ONLY 1 possibility in the same time (only file, or only metadata).
 	 * "Multipart" is not supported!
-	 * 
+	 *
 	 * @param userLogin
-	 * @param collectionURL - could be link to the collection (from the service document) 
+	 * @param collectionURL - could be link to the collection (from the service document)
 	 * 		  or a link to edit the collection ("Location" field in the response)
 	 * @param mimeFormat - use e.g. {@code "application/atom+xml"} or {@code "application/zip"}
 	 * @param packageFormat - see {@link UriRegistry.PACKAGE_SIMPLE_ZIP} or {@linkplain UriRegistry.PACKAGE_BINARY}
 	 * @param file
 	 * @param metadataMap
-	 * @return "Location" parameter from the response in case of {@code SwordRequestType.DEPOSIT} request, 
+	 * @return "Location" parameter from the response in case of {@code SwordRequestType.DEPOSIT} request,
 	 *  	   "StatusCode" parameter from the response in case of {@code SwordRequestType.REPLACE} request,
 	 *  	   or {@code null} in case of error
 	 */
 	protected SwordResponse publishElement(String collectionURL, SwordRequestType swordRequestType, String mimeFormat, String packageFormat, File file, Map<String, List<String>> metadataMap) {
-		
-		// Check if only 1 parameter is used (metadata OR file). 
+
+		// Check if only 1 parameter is used (metadata OR file).
 		// Multipart is not supported.
 		if( ((file != null)&&(metadataMap != null)) || ((file == null)&&(metadataMap == null)) ) {
-			return null; 
+			return null;
 		}
-		
+
 		FileInputStream fis = null;
-		
+
 		Deposit deposit = new Deposit();
-		
+
 		try {
 			// Check if "metadata as a Map"
 			if(metadataMap != null) {
@@ -179,19 +187,19 @@ public class SwordRepositoryExporter {
 				}
 				deposit.setEntryPart(ep);
 			}
-			
+
 			// Check if "file"
 			if(file != null) {
 				fis = new FileInputStream(file); // open FileInputStream
-				deposit.setFile(fis);				
-				deposit.setFilename(file.getName()); 	// deposit works properly ONLY with a "filename" parameter 
+				deposit.setFile(fis);
+				deposit.setFilename(file.getName()); 	// deposit works properly ONLY with a "filename" parameter
 														// --> in curl: -H "Content-Disposition: filename=file.zip"
 			}
-			
+
 			deposit.setMimeType(mimeFormat);
 			deposit.setPackaging(packageFormat);
 			deposit.setInProgress(true);
-			
+
 			switch (swordRequestType) {
 			case DEPOSIT:
 				DepositReceipt receipt = swordClient.deposit(collectionURL, deposit, authCredentials);
@@ -200,16 +208,17 @@ public class SwordRepositoryExporter {
 				SwordResponse response = swordClient.replace(collectionURL, deposit, authCredentials);
 				return response; //returns the Sword response
 			default:
-				log.error("Wrong SWORD-request type: " + swordRequestType + " : Supported here types are: " + SwordRequestType.DEPOSIT + ", " + SwordRequestType.REPLACE);
-				return null;					
+				log.error("Wrong SWORD-request type: {} : Supported here types are: {}, {}",
+						swordRequestType, SwordRequestType.DEPOSIT, SwordRequestType.REPLACE);
+				return null;
 			}
-			
+
 		} catch (FileNotFoundException e) {
-			log.error("Exception by accessing a file: " + e.getClass().getSimpleName() + ": " + e.getMessage());
-			return null;	
-		
+			log.error("Exception by accessing a file", e);
+			return null;
+
 		} catch (SWORDClientException | SWORDError | ProtocolViolationException e) {
-			log.error("Exception by making deposit: " + e.getClass().getSimpleName() + ": " + e.getMessage());
+			log.error("Exception by making deposit", e);
 			return null;
 		} finally {
 			// Close FileInputStream
@@ -217,10 +226,10 @@ public class SwordRepositoryExporter {
 				try {
 					fis.close();
 				} catch (IOException e) {
-					log.error("Exception by closing the FileInputStream: " + e.getClass().getSimpleName() + ": " + e.getMessage());
+					log.error("Exception by closing the FileInputStream", e);
 				}
 			}
 		}
 	}
-	
+
 }
