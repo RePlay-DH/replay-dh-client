919735f84dd1b6d81536b3f94e236836c9727609
Volodymyr Kushnarenko <volodymyr.kushnarenko@uni-ulm.de>
2018-11-08 11:57:35 +0100

    ExportRepository common interface: finish refactoring, ready for usage

--- src/main/java/bwfdm/replaydh/workflow/export/dspace/refactor/ExportRepository.java
+++ src/main/java/bwfdm/replaydh/workflow/export/dspace/refactor/ExportRepository.java
@@ -8,12 +8,11 @@ import java.util.Map;
 public interface ExportRepository {
 	
 	//TODO: use this interface as a common interface for each export repository. Move it to package with SwordExporter.
-
 	
 	/**
 	 * Check if export repository is accessible via API.
 	 * 
-	 * @return {@code true} if repository is accessible and {@code false} otherwise
+	 * @return {@code true} if repository is accessible and {@code false} otherwise.
 	 */
 	public boolean isRepositoryAccessible();
 		
@@ -22,10 +21,10 @@ public interface ExportRepository {
 	 * Check if current authentication credentials (e.g. API token, user login and password) are registered 
 	 * in the export repository.
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
 	 * e.g. via class constructor.
 	 * 
-	 * @return {@code true} if credentials are registered and {@code false} otherwise 
+	 * @return {@code true} if credentials are registered and {@code false} otherwise.
 	 */
 	public boolean hasRegisteredCredentials();
 	
@@ -34,11 +33,11 @@ public interface ExportRepository {
 	 * Check if current authentication credentials (e.g. API token, user login and password) 
 	 * are assigned to export in the repository.
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
 	 * e.g. via class constructor.
 	 *
 	 * @return {@code true} if count of user available collections is great than zero, 
-	 * 		   otherwise {@code false} 
+	 * 		   otherwise {@code false}. 
  	 */
 	public boolean hasAssignedCredentials();
 	
@@ -47,82 +46,97 @@ public interface ExportRepository {
 	 * Get collections, which are available for the current authentication credentials.
 	 * Could be, that different credentials can have an access to different collections.
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
 	 * e.g. via class constructor.
 	 *  
-	 * @return {@link Map} of Strings, where key="Collection full URL", value="Collection title". 
-	 * 		The map can be also empty if there ara not available collections. 
-	 * 		In case of error should be returned {@code null} value.  
+	 * @return {@link Map} of Strings, where key = "collection full URL", value = "collection title". 
+	 * 		The map can be also empty if there are not available collections. 
+	 * 		In case of some error should be returned a {@code null} value.  
 	 */
 	public Map<String, String> getAvailableCollections();
 	
 	
 	/**
-	 * Create a new item with metadata only (without any file) in some collection, which is available 
+	 * Export (create) a new entry with metadata only (without any file) in some collection, which is available 
 	 * for the current authentication credentials. Metadata are described as a {@link java.util.Map}. 
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
 	 * e.g. via class constructor.
 	 *  
-	 * @param collectionURL the full URL of the collection, where the export (ingest) will be done
-	 * @param metadataMap metadata as {@link Map}, where key = metadata field (e.g. creator, title, identifier, ... ), 
+	 * @param collectionURL the full URL of the collection, where the export (ingest) will be done.
+	 * @param metadataMap metadata as {@link Map}, where key = metadata field (e.g. "creator", "title", "year", ... ), 
 	 * 		value = {@link List} with the metadata field values (e.g. {"Author-1", "Author-2", ... }).
 	 * 
 	 * @return {@link String} with the URL of the new created entry or {@code null} in case of error.  
 	 */
-	public String createNewEntryWithMetadata(String collectionURL, Map<String, List<String>> metadataMap);
+	public String exportNewEntryWithMetadata(String collectionURL, Map<String, List<String>> metadataMap);
 	
 	
 	/**
-	 * Create a new item with metadata only (without any file) in some collection, which is available 
+	 * Export (create) a new entry with metadata only (without any file) in some collection, which is available 
 	 * for the current authentication credentials. Metadata are described as a xml-file.
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
 	 * e.g. via class constructor.
 	 * 
-	 * @param collectionURL
-	 * @param metadataFileXml
+	 * @param collectionURL the full URL of the collection, where the export (ingest) will be done.
+	 * @param metadataFileXml metadata as a xml-file in dublin core (DC) format.
 	 * 
 	 * @return {@link String} with the URL of the new created entry or {@code null} in case of error.
 	 */
-	//TODO: implement in future releases 
-	//public String createNewEntryWithMetadata(String collectionURL, File metadataFileXml) throws IOException;
+	//TODO: activate in future releases
+	//TODO: differentiate between dublin core (DC) and METS formats
+	//public String exportNewEntryWithMetadata(String collectionURL, File metadataFileXml) throws IOException;
 	
 	
 	/**
-	 * Create a new item with a file and metadata in some collection, which is available 
+	 * Export (create) a new entry with a file and metadata in some collection, which is available 
 	 * for the current authentication credentials. Metadata are described as a {@link java.util.Map}. 
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
 	 * e.g. via class constructor.
 	 * 
 	 * @param collectionURL the full URL of the collection, where the export (ingest) will be done 
-	 * @param file a file as a package with one or more files inside (e.g. zip-file) or a binary file 
-	 * 			which will be exported
-	 * @param unpackZip a flag which decides, if zip-file will be unpacked in the repository (unpackZip = {@code true},
-	 * 			a new item will include in this case all files of the zip-archive) or zip-file will be exported 
-	 * 			as a binary file (unpackZip = {@code false}, a new item will include only 1 file - the zip-archive 
-	 * 			as a binary file). 
-	 * @param metadataMap
+	 * @param file an archive file with one or more files inside (e.g. ZIP-file as a standard) or a binary file 
+	 * 			which will be exported.
+	 * @param unpackFileIfArchive should be used for archive files (e.g. ZIP). A flag which decides, 
+	 * 			if the exported archive will be unpacked in the repository ({@code true} value,
+	 * 			new entry will include in this case all files of the archive file) or archive will be exported 
+	 * 			as a binary file ({@code false} value, new entry will include only 1 file - the exported archive
+	 * 			as a binary file). <b>NOTE:</b> if unpacking is not supported by the repository, 
+	 * 			please use {@code false} value. 
+	 * @param metadataMap metadata as {@link Map}, where key = metadata field (e.g. "creator", "title", "year", ... ), 
+	 * 		value = {@link List} with the metadata field values (e.g. {"Author-1", "Author-2", ... }). 
 	 * 
 	 * @return {@link String} with the URL of the new created entry or {@code null} in case of error.
 	 */
-	public String createNewEntryWithFileAndMetadata(String collectionURL, File file, boolean unpackZip, Map<String, List<String>> metadataMap) throws IOException;
+	public String exportNewEntryWithFileAndMetadata(String collectionURL, File file, 
+				boolean unpackFileIfArchive, Map<String, List<String>> metadataMap) throws IOException;
 	
 	
 	/**
-	 * Create a new item with a file and metadata in some collection, which is available for the current authentication credentials.
-	 * Metadata are described in the xml-file.
+	 * Export (create) a new entry with a file and metadata in some collection, which is available 
+	 * for the current authentication credentials. Metadata are described as a xml-file.
 	 * <p>
-	 * IMPORTANT: credentials are used implicitly. Definition of the credentials must be done in other place, e.g. via class constructor.
+	 * <b>IMPORTANT:</b> credentials are used implicitly. Definition of the credentials must be done in other place, 
+	 * e.g. via class constructor.
 	 * 
-	 * @param collectionURL
-	 * @param file
-	 * @param metadataFileXml
-	 * @return {@code true} in case of success and {@code false} otherwise
+	 * @param collectionURL the full URL of the collection, where the export (ingest) will be done.
+	 * @param file an archive file with one or more files inside (e.g. ZIP-file as a standard) or a binary file 
+	 * 			which will be exported.
+	 * @param unpackFileIfArchive should be used for archive files (e.g. ZIP). A flag which decides, 
+	 * 			if the exported archive will be unpacked in the repository ({@code true} value,
+	 * 			new entry will include in this case all files of the archive file) or archive will be exported 
+	 * 			as a binary file ({@code false} value, new entry will include only 1 file - the exported archive
+	 * 			as a binary file). <b>NOTE:</b> if unpacking is not supported by the repository, 
+	 * 			please use {@code false} value.
+	 * @param metadataFileXml metadata as a xml-file in dublin core (DC) format.
 	 * 
-	 * TODO: return String with the new item URL in case of success or "" otherwise 
+	 * @return {@link String} with the URL of the new created entry or {@code null} in case of error. 
 	 */
-	public boolean createNewEntryWithFileAndMetadata(String collectionURL, File file, File metadataFileXml) throws IOException;
+	//TODO: activate in future releases
+	//TODO: differentiate between dublin core (DC) and METS formats
+	//public String exportNewEntryWithFileAndMetadata(String collectionURL, File file, 
+	//			boolean unpackFileIfArchive, File metadataFileXml) throws IOException;
 	
 }
