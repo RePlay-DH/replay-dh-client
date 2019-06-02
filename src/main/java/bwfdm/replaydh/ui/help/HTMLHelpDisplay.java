a64997c4f824101b5f5c78780c500a53d6849848
Markus Gärtner <mcgaerty@ikarus-systems.net>
2019-06-02 20:30:50 +0200

    Adjust HTMLHelpDisplay to use try-with-resource when loading text.

--- src/main/java/bwfdm/replaydh/ui/help/HTMLHelpDisplay.java
+++ src/main/java/bwfdm/replaydh/ui/help/HTMLHelpDisplay.java
@@ -1,7 +1,5 @@
 package bwfdm.replaydh.ui.help;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -25,32 +23,27 @@ import bwfdm.replaydh.io.IOUtils;
 import bwfdm.replaydh.resources.ResourceManager;
 
 public class HTMLHelpDisplay {
-	
+
 	public HTMLHelpDisplay() {
 	}
-	
+
 	private static final Logger log = LoggerFactory.getLogger(HTMLHelpDisplay.class);
-	
-	private String document = null;
+
+	private String document = null; //TODO do we even need the raw string content after initial parsing?
 	private Document doc;
-	private ResourceManager rm = ResourceManager.getInstance();
-	
+
 	/**
 	 * Reads the HTML Help file
 	 */
 	public void readHelpFile() {
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		File markdownFile = new File(classloader.getResource("bwfdm/replaydh/help/client-docu.html").getFile());
-		InputStream input = null;
-		try {
-			input = new FileInputStream(markdownFile);
-			document=IOUtils.readStream(input);
+		try(InputStream input = HTMLHelpDisplay.class.getResourceAsStream("bwfdm/replaydh/help/client-docu.html")) {
+			document = IOUtils.readStream(input);
 		} catch (IOException e) {
 			log.error("Error reading markdown file as stream",e);
 		}
 		doc = Jsoup.parse(document);
 	}
-	
+
 	/**
 	 * finds the position of a h3 tag
 	 * @param id of the h3 tag
@@ -70,16 +63,16 @@ public class HTMLHelpDisplay {
 		}
 		return section;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param anchor
 	 * @param comp
 	 */
 	public void showHelpSection(String anchor, JComponent comp) {
 		String section = findAndPrintPosition(anchor);
 		JFrame frame = new JFrame();
-		frame.setTitle(rm.get("replaydh.documentation.helpWindow.title"));
+		frame.setTitle(ResourceManager.getInstance().get("replaydh.documentation.helpWindow.title"));
 		JEditorPane editorPane = new JEditorPane();
 		URL jarLocation = HTMLHelpDisplay.class.getProtectionDomain().getCodeSource().getLocation();
 		URL baseURL;
